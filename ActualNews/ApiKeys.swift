2d6f940e978f02b8bd177c00653864efe66d0a45
Егор Никитин <Egornik1994@yandex.ru>
2021-06-10 14:03:45 +0300

    Added api key

---
+++ ActualNews/ApiKeys.swift
@@ -0,0 +1,12 @@
+//
+//  ApiKeys.swift
+//  ActualNews
+//
+//  Created by Егор Никитин on 10.06.2021.
+//
+
+import Foundation
+
+enum ApiKeys: String {
+    case newsApiKey = "41a6adabd8de4b5ea9b31cdfac93657f"
+}
