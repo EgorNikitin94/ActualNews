51d57af25aa8b0709b880bfc3434b683d54300c6
Егор Никитин <Egornik1994@yandex.ru>
2021-06-10 13:47:26 +0300

    Initial Commit

---
+++ ActualNews/ActualNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActualNewsApp.swift
+//  ActualNews
+//
+//  Created by Егор Никитин on 10.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ActualNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActualNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ActualNews
+//
+//  Created by Егор Никитин on 10.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
